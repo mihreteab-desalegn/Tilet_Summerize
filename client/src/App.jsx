17ad2ea81622ae81d3b588c1f684862eaf1fec6e
mihreteab-desalegn <mihreteab31@gmail.com>
2025-06-28 20:19:40 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
           content="Tilet is a cutting-edge AI-powered platform designed to revolutionize the way you learn from YouTube videos. Instantly generate concise, easy-to-understand summaries and create interactive quizzes tailored to the video content, helping students, educators, and knowledge seekers save time, improve comprehension, and retain information more effectively. Whether you're studying for exams, preparing lessons, or simply curious, Tilet transforms lengthy video content into digestible learning tools, boosting productivity and engagement for learners of all levels."
         />
         <meta property="og:image" content="./video.jpg" />
-        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:url" content="https://tilet-summerize.onrender.com" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
